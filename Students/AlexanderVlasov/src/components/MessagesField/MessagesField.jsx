7255ef2b515befd1774e6fa349bb131e15a61998
ASVVlasov <asvvlasov@gmail.com>
2020-02-28 22:00:53 +0300

    что-то получилось)

--- Students/AlexanderVlasov/src/components/MessagesField/MessagesField.jsx
+++ Students/AlexanderVlasov/src/components/MessagesField/MessagesField.jsx
@@ -1,12 +1,29 @@
 import React, {Component } from 'react';
 import ReactDom from 'react-dom';
-import './style.css';
+import {withStyles} from '@material-ui/core/styles';
+import styles from './style.css';
 
 import Message from '../Message/Message.jsx';
-import { Input, Fab, TextField } from '@material-ui/core';
+import { Box, Fab, TextField, GridList } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send'; 
 
-export default class Messages extends Component {
+const useStyles = (theme => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      overflow: 'hidden',
+      backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+      width: '100%',
+      maxHeight: 'calc(100vh - 120px)',
+    }
+  }));
+
+class Messages extends Component {
+
+    messagesEndRef = React.createRef()
+
     constructor(props) {
         super(props);
         this.user = props.usr;
@@ -15,6 +32,34 @@ export default class Messages extends Component {
                 {
                     user: 'Alex',
                     text: 'hello'
+                },
+                {
+                    user: null,
+                    text: null
+                },
+                {
+                    user: 'Anna',
+                    text: 'Hi'
+                },
+                {
+                    user: null,
+                    text: null
+                },
+                {
+                    user: 'Alex',
+                    text: 'hello'
+                },
+                {
+                    user: null,
+                    text: null
+                },
+                {
+                    user: 'Anna',
+                    text: 'Hi'
+                },
+                {
+                    user: null,
+                    text: null
                 },
                 {
                     user: null,
@@ -39,6 +84,10 @@ export default class Messages extends Component {
             this.newMessage();
     }
 
+    componentDidMount () {
+        this.scrollToBottom()
+    }
+
     componentDidUpdate() {
         const msgs = this.state.messages;
         if (msgs.length % 2 === 1) {
@@ -50,6 +99,7 @@ export default class Messages extends Component {
                 );
             }, 500);
         }
+        this.scrollToBottom()
     }
 
     newMessage = () => {
@@ -61,29 +111,36 @@ export default class Messages extends Component {
         );
     }
 
+    scrollToBottom = () => {
+        this.messagesEndRef.current.lastElementChild.scrollIntoView({ behavior: 'smooth' })
+    }
+
     render() {
+        const { classes } = this.props;
         const renderMessages = this.state.messages.slice().map((obj, index) => {
             return <Message key={index} sender={obj.user} text={obj.text}/>
         })
         return (
-            <div className="wrapper flex-grow-1">
-                {renderMessages}
-                <div className="d-flex">
-                    <TextField 
-                        className="flex-grow-1"
-                        label="Новое сообщение"
-                        value={this.state.msg}
-                        onChange={this.handleChanges} 
-                        onKeyUp={this.handleChanges}
-                        variant="outlined"
-                    />
-                    <Fab 
-                        color="primary" 
-                        onClick={this.newMessage} >
-                        <SendIcon />
-                    </Fab>
-                </div>
+            <div className={ classes.root}>
+                <GridList className={ classes.gridList } cols={ 1 } spacing={ 0 } ref={this.messagesEndRef}>
+                    {renderMessages}
+                </GridList>
+                <TextField 
+                    className="flex-grow-1"
+                    label="Новое сообщение"
+                    value={this.state.msg}
+                    onChange={this.handleChanges} 
+                    onKeyUp={this.handleChanges}
+                    variant="outlined"
+                />
+                <Fab 
+                    color="primary" 
+                    onClick={this.newMessage} >
+                    <SendIcon />
+                </Fab>
             </div>
         )
     }
 }
+
+export default withStyles(useStyles)(Messages)
--- Students/AlexanderVlasov/src/index.jsx
+++ Students/AlexanderVlasov/src/index.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDom from "react-dom";
-import { ThemeProvider, createMuiTheme, CssBaseline, Container, Fab } from "@material-ui/core";
-import AddIcon from '@material-ui/icons/Add';
+import { ThemeProvider, createMuiTheme, CssBaseline, Container, Fab, Grid } from "@material-ui/core";
 
 import '../node_modules/bootstrap/dist/css/bootstrap-grid.css';
 import 'typeface-roboto';
 
 import Messages from './components/MessagesField/MessagesField.jsx';
 import Header from './components/Header/Header.jsx';
+import ChatList from './components/ChatList/ChatList.jsx';
 
 const theme = createMuiTheme({
     typography: {
@@ -32,15 +32,20 @@ ReactDom.render(
         <CssBaseline></CssBaseline>
         <Container maxWidth="lg">
             <Header usr={ user }/>
-            <div className="d-flex">
-                <div className="chat-list">
-                <Fab color="primary" aria-label="add">
-                    <AddIcon />
-                </Fab>
-                Создать чат
-                </div>
-                <Messages usr={ user }/>
-            </div>
+            <Grid
+                container
+                direction="row"
+                justify="space-between"
+                alignItems="flex-start"
+                spacing={1}
+            >
+                <Grid item xs={3}>
+                    <ChatList />
+                </Grid>
+                <Grid item xs={9}>
+                    <Messages usr={ user }/>
+                </Grid>
+            </Grid>
         </Container>
     </ThemeProvider>, 
     document.getElementById("app")
