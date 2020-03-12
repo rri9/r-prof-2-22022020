53cdb4f643f54ca5fb49cca2fec95425874d926d
Maxim Sozinov <max.sozinov@gmail.com>
2020-03-12 21:44:37 +0700

    before connecting router to redux

--- Students/Maxim Sozinov/src/middlewares/messageMiddleware.js
+++ Students/Maxim Sozinov/src/middlewares/messageMiddleware.js
@@ -3,7 +3,7 @@ import { SEND_MSG, sendMessage } from '../store/actions/messages_actions.js';
 export default store => next => (action) => {
    switch (action.type) {
       case SEND_MSG: {
-         if ( action.sender === 'Darth Vader') {
+         if ( action.sender === store.getState().userReducer.user ) {
             const messageId = action.messageId + 1;
             setTimeout(() => store.dispatch ( sendMessage ( 
                   messageId, 
--- Students/Maxim Sozinov/src/store/reducers/index.js
+++ Students/Maxim Sozinov/src/store/reducers/index.js
@@ -2,5 +2,10 @@ import { combineReducers } from 'redux';
 
 import msgReducer from './messages_reducer.js';
 import chatsReducer from './chats_reducer.js';
+import userReducer from './user_reducer.js';
 
-export default combineReducers({ msgReducer, chatsReducer});
+export default combineReducers({ 
+      msgReducer,
+      chatsReducer,
+      userReducer,
+   });
