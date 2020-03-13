adafa4b5fe02d7a4b5056fe64ee446690d920f28
Maxim Sozinov <max.sozinov@gmail.com>
2020-03-13 14:13:15 +0700

    1

--- Students/Maxim Sozinov/src/store/store.js
+++ Students/Maxim Sozinov/src/store/store.js
@@ -1,12 +1,22 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { createBrowserHistory } from 'history';
+import { routerMiddleware } from 'connected-react-router';
+
 import initialReducers from './reducers';
 
-export default function initStore () {
+export const history = createBrowserHistory();
+
+function initStore () {
    let initialStore = {};
 
    return createStore( 
-      initialReducers, 
+      initialReducers( history ), 
       initialStore,
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+      compose(
+         applyMiddleware( routerMiddleware(history) ),
+         window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : () => {},
+      )
    );
 }
+
+export default initStore;
