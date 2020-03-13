6c24e8ed45200a9a0ba2b607207db37a3799ebd8
Maxim Sozinov <max.sozinov@gmail.com>
2020-03-13 15:41:50 +0700

    persistor added

--- Students/Maxim Sozinov/src/index.jsx
+++ Students/Maxim Sozinov/src/index.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import Router from './router/Router.jsx';
 
 import { ConnectedRouter } from 'connected-react-router';
+import { PersistGate } from 'redux-persist/integration/react'; 
 
 import { Provider } from 'react-redux';
 import initStore, { history } from './store/store';
@@ -13,20 +14,23 @@ import { Container, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './main.css';
 
+const { store, persistor } = initStore(); 
 
 ReactDom.render(
 
-    <Provider store={initStore()}>
-        <ConnectedRouter history={history}>
-            <Container className="vh-100 overflow-hidden d-flex flex-column justify-content-between">
-                <Row className="header p-2 rounded-top shadow">
-                    <Link to={'/'}><h1>First React App</h1></Link>
-                </Row>
-
-                <Router />
-
-            </Container>
-        </ConnectedRouter>
+    <Provider store={store}>
+        <PersistGate loading={ null } persistor={ persistor }>
+            <ConnectedRouter history={history}>
+                <Container className="vh-100 overflow-hidden d-flex flex-column justify-content-between">
+                    <Row className="header p-2 rounded-top shadow">
+                        <Link to={'/'}><h1>First React App</h1></Link>
+                    </Row>
+    
+                    <Router />
+    
+                </Container>
+            </ConnectedRouter>
+        </PersistGate>
     </Provider>
     ,
     document.getElementById('app')
--- Students/Maxim Sozinov/src/store/store.js
+++ Students/Maxim Sozinov/src/store/store.js
@@ -2,22 +2,40 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { createBrowserHistory } from 'history';
 import { routerMiddleware } from 'connected-react-router';
 
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; 
+
 import initialReducers from './reducers';
 import middlewares from '../middlewares/index.js';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+
+const persistConfig = { 
+   key: 'reactmessanger', 
+   storage, 
+   stateReconciler: autoMergeLevel2, 
+   whitelist: [    
+      'msgReducer',
+      'chatsReducer',
+      'userReducer',
+   ], 
+};
 
 export const history = createBrowserHistory();
 
 function initStore () {
    let initialStore = {};
 
-   return createStore( 
-      initialReducers( history ), 
+   const store = createStore(
+      persistReducer( persistConfig, initialReducers( history ) ), 
       initialStore,
       compose(
          applyMiddleware( routerMiddleware(history), ...middlewares ),
          window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : () => {},
       )
    );
+   const persistor = persistStore(store);
+
+   return { store, persistor }; 
 }
 
 export default initStore;
