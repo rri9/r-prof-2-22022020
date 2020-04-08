0a4d7b00d8011999c5a6cf03cf496555aaf0552f
rri9 <rri9@mail.ru>
2020-04-08 23:15:29 +0300

    Server: messages array into each chat

--- Students/Ruslan_Rakushov/server/Models/chat.js
+++ Students/Ruslan_Rakushov/server/Models/chat.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+// const Message = require('./message');
+
+const messageSchema = new Schema({
+  sender: { type: String, required: true },
+  text:   { type: String, required: true },
+});
 
 const chatSchema = new Schema({
   title: { type: String, required: true },
-  messages: { type: Array},
+  messages: [messageSchema],
 });
 
 module.exports = mongoose.model('chat', chatSchema);
