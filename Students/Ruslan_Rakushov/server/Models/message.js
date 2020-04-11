eee3c5c644cf4755ed2f269e07981a4cc68b65d4
rri9 <rri9@mail.ru>
2020-04-11 16:07:09 +0300

    Server: message author identified by user id

--- Students/Ruslan_Rakushov/server/Models/message.js
+++ Students/Ruslan_Rakushov/server/Models/message.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const messageSchema = new Schema({
-  sender: { type: String, required: true },
+  senderId: { type: String, required: true },
   text:   { type: String, required: true },
 });
 
