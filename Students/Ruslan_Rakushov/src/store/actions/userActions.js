aa27134803828c73d859f96ae0eb0c6121bb4b9f
rri9 <rri9@mail.ru>
2020-04-12 17:30:28 +0300

    add registration actions/reducers

--- Students/Ruslan_Rakushov/src/store/actions/userActions.js
+++ Students/Ruslan_Rakushov/src/store/actions/userActions.js
@@ -4,6 +4,11 @@ export const USER_LOGIN_START = '@@profile/USER_LOGIN_START';
 export const USER_LOGIN_SUCCESS = '@@profile/USER_LOGIN_SUCCESS';
 export const USER_LOGIN_ERROR = '@@profile/USER_LOGIN_ERROR';
 
+export const USER_REGISTRATION_START = '@@profile/USER_REGISTRATION_START';
+export const USER_REGISTRATION_SUCCESS = '@@profile/USER_REGISTRATION_SUCCESS';
+export const USER_REGISTRATION_ERROR = '@@profile/USER_REGISTRATION_ERROR';
+
+//----------login
 export const login = (email, password) => {
   return async (dispatch) => {
     dispatch(loginStart());
@@ -26,18 +31,48 @@ export const login = (email, password) => {
     }
   };
 };
- 
 export const loginStart = () => ({
   type: USER_LOGIN_START,
 });
-
 export const loginSuccess = (user) => ({
   type: USER_LOGIN_SUCCESS,
   payload: user
 });
-
 export const loginError = (error) => ({
   type: USER_LOGIN_ERROR,
   payload: error
 });
- 
+
+//---registration
+export const registration = (name, email, age, password) => {
+  return async (dispatch) => {
+    dispatch(registrationStart());
+
+    let response = await fetch('/api/registration', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name: name, email: email, age: age, password: password })
+    });
+    let result = await response.json();
+
+    if (response.status === 201) {
+      dispatch(registrationSuccess());
+      dispatch(push('/login'));
+    } else {
+      dispatch(registrationError(result.error));
+    }
+  }
+}
+export const registrationStart = () => ({
+  type: USER_REGISTRATION_START,
+});
+export const registrationSuccess = () => ({
+  type: USER_REGISTRATION_SUCCESS,
+});
+export const registrationError = (error) => ({
+  type: USER_REGISTRATION_ERROR,
+  payload: error
+});
+//---------------
--- Students/Ruslan_Rakushov/src/store/reducers/userReducers.js
+++ Students/Ruslan_Rakushov/src/store/reducers/userReducers.js
@@ -1,32 +1,53 @@
 import update from 'immutability-helper';
 import {
   USER_LOGIN_START, USER_LOGIN_SUCCESS, USER_LOGIN_ERROR,
+  USER_REGISTRATION_START, USER_REGISTRATION_SUCCESS, USER_REGISTRATION_ERROR,
 } from '../actions/userActions.js';
 
 const initialStore = {
   user: {},
   isLoading: false,
   userLoginError: '',
+  userRegistrationError: '',
+  userRegistrationMessage: '',
 };
 
 export default function userReducers(store = initialStore, action) {
   switch (action.type) {
     case USER_LOGIN_START:
       return update(store, {
-        isLoading: { $set: true }
+        isLoading: { $set: true },
+        userLoginError: { $set: '' },
       });
-    //-------------------
     case USER_LOGIN_SUCCESS:
       return update(store, {
         isLoading: { $set: false },
         userLoginError: { $set: '' },
-        user: { $set: action.payload},
+        user: { $set: action.payload },
+        userRegistrationMessage: { $set: '' },
       });
-    //-------------------
     case USER_LOGIN_ERROR:
       return update(store, {
         isLoading: { $set: false },
         userLoginError: { $set: action.payload },
+        userRegistrationMessage: { $set: '' },
+      });
+    //-------------------
+    case USER_REGISTRATION_START:
+      return update(store, {
+        isLoading: { $set: true },
+        userRegistrationError: { $set: '' },
+      });
+    case USER_REGISTRATION_SUCCESS:
+      return update(store, {
+        isLoading: { $set: false },
+        userRegistrationError: { $set: '' },
+        userRegistrationMessage: { $set: 'Вы успешно зарегистрировались. Введите адрес электронной почты и пароль, указанные при регистрации' },
+      });
+    case USER_REGISTRATION_ERROR:
+      return update(store, {
+        isLoading: { $set: false },
+        userRegistrationError: { $set: action.payload },
       });
     //-------------------
     default:
