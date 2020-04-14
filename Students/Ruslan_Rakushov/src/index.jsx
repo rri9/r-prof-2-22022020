ba12e8cc3de0ad7f7a622d4fa9bf0a933bc875ac
rri9 <rri9@mail.ru>
2020-04-14 17:33:20 +0300

    save user info locally (redux-persist)

--- Students/Ruslan_Rakushov/src/index.jsx
+++ Students/Ruslan_Rakushov/src/index.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
+import { PersistGate } from 'redux-persist/es/integration/react';
 import initStore, { history } from './store/store.js';
 
 import Layout from './Components/Layout/Layout.jsx';
 
+const { store, persistor } = initStore();
+
 ReactDOM.render(
-  <Provider store={ initStore() }>
-    <ConnectedRouter history={history}>
-      <Layout />
-    </ConnectedRouter>
-  </Provider>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <ConnectedRouter history={history}>
+        <Layout />
+      </ConnectedRouter>
+    </PersistGate >
+  </Provider >
   ,
   document.getElementById('app'),
 );
--- Students/Ruslan_Rakushov/src/store/store.js
+++ Students/Ruslan_Rakushov/src/store/store.js
@@ -2,17 +2,32 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import { createBrowserHistory } from 'history';
 import { routerMiddleware } from 'connected-react-router';
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
 import initReducers from './reducers/index.js';
 import middlewares from './middlewares/index.js';
+
+const rootPersistConfig = {
+  key: 'ReactGram_v2',
+  storage,
+  stateReconciler: autoMergeLevel2,
+  whitelist: ['userReducers'],
+}
+// const userPersistConfig = {
+//   key: 'userReducers',
+//   storage,
+//   whitelist: ['user']
+// }
 
 export const history = createBrowserHistory();
 
 function initStore() {
   const innitialStore = {};
 
-  return createStore(
-    initReducers(history),
+  const store = createStore(
+    persistReducer(rootPersistConfig, initReducers(history)),
     innitialStore,
     compose(
       applyMiddleware(
@@ -22,7 +37,10 @@ function initStore() {
       ),
       window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
     ),
-   );
+  );
+
+  const persistor = persistStore(store);
+  return { store, persistor };
 }
 
 export default initStore;
